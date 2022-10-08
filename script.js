e5cc1cf8654d4288bf9df61171bba10ba24e107d
Jamie Hoyer <hoyer307@gmail.com>
2022-10-08 15:52:18 +0200

    Added querySelector for the Number buttons

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Query Selector for Number btns
+const btnZero = document.querySelector(".btn-zero");
+const btnOne = document.querySelector(".btn-one");
+const btnTwo = docuemnt.querySelector(".btn-two");
+const btnThree = document.querySelector(".btn-three");
+const btnFour = document.querySelector(".btn-four");
+const btnFive = document.querySelector(".btn-five");
+const btnSix = document.querySelector(".btn-six");
+const btnSeven = document.querySelector(".btn-seven");
+const btnEight = document.querySelector(".btn-eight");
+const btnNine = document.querySelector(".btn-nine");
