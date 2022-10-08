527d3a70dda7bdc2f84c5e1f696e3ccb4d99a589
Jamie Hoyer <hoyer307@gmail.com>
2022-10-08 15:56:53 +0200

    Added querySelector for the other buttons

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Query Selector for Number btns
+// Query Selector for number buttons
 const btnZero = document.querySelector(".btn-zero");
 const btnOne = document.querySelector(".btn-one");
 const btnTwo = docuemnt.querySelector(".btn-two");
@@ -9,3 +9,12 @@ const btnSix = document.querySelector(".btn-six");
 const btnSeven = document.querySelector(".btn-seven");
 const btnEight = document.querySelector(".btn-eight");
 const btnNine = document.querySelector(".btn-nine");
+
+// Query Selector for other buttons
+const clearBtn = document.querySelector(".btn-clear");
+const divideBtn = document.querySelector(".btn-divide");
+const multiplyBtn = document.querySelector(".btn-multiply");
+const minusBtn = document.querySelector(".btn-minus");
+const dotBtn = document.querySelector(".btn-dot");
+const equalsBtn = document.querySelector(".btn-equals");
+const plusBtn = document.querySelector(".btn-plus");
